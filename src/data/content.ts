f69572890de9f0057d2d3c74172e56f465415560
yuand8899 <yuand8899@gmail.com>
2026-08-08 21:10:21 -0700

    Update content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -33,7 +33,7 @@ export const site = {
     zh: 'Unit 203 1548 Johnston Road, White Rock, BC, V4B 3Z7',
   },
   legalName: {
-    en: 'SourcePoint Wellness',
+    en: 'SourcePoint Acupuncture',
     zh: '源真中医',
   },
 };
